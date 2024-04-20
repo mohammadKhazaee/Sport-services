650074af1fa20fa497a8fe68176c382b304752c9
mohammadKhazaee <m.khazae.p@gmail.com>
2024-04-20 16:09:24 +0430

    added morgan and changes post login succeful response body

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const sequelize = require('./utils/database')
 const multer = require('multer')
-// const swaggerUI = require('swagger-ui-express')
-// const swaggerJsDoc = require('swagger-jsdoc')
 const { v4: uuidv4 } = require('uuid')
 require('dotenv').config()
 const helmet = require('helmet')
+const morgan = require('morgan')
 const compression = require('compression')
 
 const routes = require('./routes/routes')
@@ -23,38 +22,6 @@ const ExerciseSession = require('./models/exerciseSession')
 
 const PORT = process.env.PORT || 3000
 const LIARA_URL = process.env.LIARA_URL || 'http://localhost:' + PORT
-
-// const options = {
-// 	definition: {
-// 		openapi: '3.0.0',
-// 		info: {
-// 			title: 'Social App API',
-// 			description: 'A simple twitter-like app',
-// 		},
-// 		servers: [
-// 			{
-// 				url: `${LIARA_URL}`,
-// 			},
-// 		],
-// 		components: {
-// 			securitySchemes: {
-// 				bearerAuth: {
-// 					type: 'http',
-// 					scheme: 'bearer',
-// 					bearerFormat: 'JWT',
-// 				},
-// 			},
-// 		},
-// 		security: [
-// 			{
-// 				bearerAuth: [],
-// 			},
-// 		],
-// 	},
-// 	apis: ['./routes/*.js', './models/*.js'],
-// }
-
-// const specs = swaggerJsDoc(options)
 
 const app = express()
 
@@ -76,9 +43,8 @@ const fileFilter = (req, file, cb) => {
 	cb(null, isValidType)
 }
 
-// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, { explorer: true }))
-
 app.use(helmet())
+app.use(morgan('combined'))
 app.use(compression())
 app.use(bodyParser.json()) // application/json
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
@@ -90,10 +56,6 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
 })
-
-// app.get('/', (req, res) => {
-// 	res.send('<a href="/api-docs" style="text-align:center;display:block">/api-docs</a>')
-// })
 
 app.use(routes)
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -69,7 +69,9 @@ exports.postLogin = async (req, res, next) => {
 		const token = jwt.sign({ userId: user.userId.toString() }, process.env.JWT_SECRET, {
 			expiresIn: '1h',
 		})
-		res.status(200).json({ token: token, userId: user.userId.toString() })
+		const { password, updatedAt, createdAt, resetToken, resetTokenExp, ...responseUser } =
+			user.dataValues
+		res.status(200).json({ token: token, user: responseUser })
 	} catch (err) {
 		if (!err.statusCode) err.statusCode = 500
 		next(err)
